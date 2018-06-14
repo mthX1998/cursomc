31bd84391e02831ad7bdd540a52ac98beb9aae78
Matheus Mancini <mancini995@gmail.com>
2018-06-13 21:27:19 -0300

    Pedido,EstadoPagamento,Pagamento - PRT1

--- src/main/java/com/mancini/cursomc/domain/Produto.java
+++ src/main/java/com/mancini/cursomc/domain/Produto.java
@@ -34,6 +34,10 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	
+	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
     public Produto(){
 		
 	}
@@ -76,6 +80,14 @@ public class Produto implements Serializable{
 
 	public void setPreco(Double preco) {
 		this.preco = preco;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
