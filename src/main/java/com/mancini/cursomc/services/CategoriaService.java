fdc6449b759cbe9bcf68a8864a1cb864d5b5f3cb
Matheus <mancini995@gmail.com>
2018-07-25 18:11:35 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/mancini/cursomc/services/CategoriaService.java
+++ src/main/java/com/mancini/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mancini.cursomc.domain.Categoria;
+import com.mancini.cursomc.dto.CategoriaDTO;
 import com.mancini.cursomc.repositories.CategoriaRepository;
 import com.mancini.cursomc.services.exceptions.DataIntegrityException;
 import com.mancini.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,5 +59,9 @@ public class CategoriaService {
     	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
     	
     	return repo.findAll(pageRequest);
+    }
+    
+    public Categoria fromDTO(CategoriaDTO objDto) {
+    	return new Categoria(objDto.getId(),objDto.getNome());
     }
 }
