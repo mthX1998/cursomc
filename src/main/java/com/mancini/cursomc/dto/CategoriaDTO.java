a67ee4ee3ad2a0dad66b8462a419c601565e6f32
Matheus <mancini995@gmail.com>
2018-07-25 20:59:07 -0300

    PUT,DELETE e GET para Cliente

--- src/main/java/com/mancini/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/mancini/cursomc/dto/CategoriaDTO.java
@@ -13,7 +13,7 @@ public class CategoriaDTO implements Serializable {
 
 	 private Integer id;
 	 
-	 @NotEmpty(message = "Preenchimento Obrigatório!")
+	 @NotEmpty(message = "Campo Obrigatório!")
 	 @Length(min=5,max=80, message = "O tamanho deve ser entre 5 e 80 caracteres!")
 	 private String nome;
 	 
