7c1dc71de57d5cf325fe4113f7fc421d88c71c03
mthX1998 <mancini995@gmail.com>
2018-08-15 11:46:31 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/mancini/cursomc/resources/ClienteResource.java
+++ src/main/java/com/mancini/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.mancini.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mancini.cursomc.domain.Cliente;
 import com.mancini.cursomc.dto.ClienteDTO;
+import com.mancini.cursomc.dto.ClienteNewDTO;
 import com.mancini.cursomc.services.ClienteService;
 
 @RestController
@@ -34,6 +37,18 @@ public class ClienteResource {
     	
     	return ResponseEntity.ok().body(obj);
     }
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
 	
 	
 	
